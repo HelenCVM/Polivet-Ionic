f822ee7edf2a20054657447fbf39c71027ceaf4c
Cinthia Iza <cizac1@est.ups.edu.ec>
2021-11-09 16:34:00 -0500

    Propietario

--- polivet/src/app/pages/propietario/propietario.page.ts
+++ polivet/src/app/pages/propietario/propietario.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Propietario } from 'src/app/Modelo/Propietario';
 
 @Component({
   selector: 'app-propietario',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class PropietarioPage implements OnInit {
 
   constructor() { }
-
+  propietario: Propietario= new Propietario();
   ngOnInit() {
   }
 
